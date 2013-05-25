e3c0bd932033e8f9a8813b360d7406c092ba11a5
Chad Jolly <cjolly@lawgical.com>
2013-05-24 23:16:37 -0600

    my first bash completion… psql!
    * probably pretty janky considering the other possible arguments to the
    command, but a  decent start

---
+++ bash/psql_completion.bash
@@ -0,0 +1,8 @@
+_psql() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  COMPREPLY=( $(compgen -W "$(psql --list --no-align --tuples-only --quiet --no-password --field-separator=$'\t' | awk 'NF > 1 { print $1 }')" -- "$word") )
+}
+
+complete -F _psql psql
--- bashrc
+++ bashrc
@@ -3,5 +3,6 @@ DOTFILES=~/bin/dotfiles
 source $DOTFILES/bash/env
 source $DOTFILES/bash/aliases
 source $DOTFILES/bash/completions
+source $DOTFILES/bash/psql_completion.bash
 source $DOTFILES/bash/path
 source $DOTFILES/bash/prompt
