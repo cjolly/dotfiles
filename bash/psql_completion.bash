0e9677f06b04f3e80aa6584db13d8a8fd0eef1f4
Chad Jolly <cjolly@lawgical.com>
2014-03-29 13:31:03 -0600

    psql tab complete: ignore _test dbs

--- bash/psql_completion.bash
+++ bash/psql_completion.bash
@@ -2,7 +2,7 @@ _psql() {
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
 
-  COMPREPLY=( $(compgen -W "$(psql --list --no-align --tuples-only --quiet --no-password --field-separator=$'\t' | awk 'NF > 1 { print $1 }')" -- "$word") )
+  COMPREPLY=( $(compgen -W "$(psql --list --no-align --tuples-only --quiet --no-password --field-separator=$'\t' | grep -v '_test' | awk 'NF > 1 { print $1 }')" -- "$word") )
 }
 
 complete -F _psql psql
